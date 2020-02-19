7b712b7901822eb5893f64b22607270741c45764
Stefan Oljacic <djedpool@gmail.com>
2020-02-19 14:40:19 +0100

    Follow/Unfollow user first time of using vue component. Fixed #11

--- app/Profile.php
+++ app/Profile.php
@@ -9,6 +9,15 @@ class Profile extends Model
 
     protected $guarded = [];
 
+    public function profileImage() {
+        $imagePath = ($this->image) ? $this->image : 'profile/llNTDD2yvEVOEh8Uykc4DImoBEbATEDjCPguilHP.jpeg';
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers() {
+        return $this->belongsToMany(User::class);
+    }
+
     public function user() {
         return $this->belongsTo(User::class); // to complete relation following convention
     }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,11 +5,14 @@
     <div class="row">
         <div class="col-3 p-5">
             {{--<img src="https://instagram.fbeg1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fbeg1-1.fna.fbcdn.net&_nc_ohc=I4amkmjG6VUAX9zbXjc&oh=5126319d5fb620b2a6ae2b47b29d6ff5&oe=5ECEBCBA" class="rounded-circle">--}}
-            <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100">
+            <img src="{{ $user->profile->profileImage() }}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-baseline">
-                <h1>{{ $user->username }}</h1>
+                <div class="d-flex align-items-center pb-4">
+                    <div class="h4">{{ $user->username }}</div>
+                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
+                </div>
                 @can('update', $user->profile)
                     <a href="/p/create">Add New Post</a>
                 @endcan
